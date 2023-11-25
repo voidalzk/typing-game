6bb63c2f444a5134b0ebadb43b17dc9c63a43ea6
Bruno_Brug <brunobrugnerottodelara@gmail.com>
2023-11-25 15:45:06 -0300

    update

--- src/views/hist.php
+++ src/views/hist.php
@@ -1,32 +1,21 @@
-<?php 
-
+<?php
 session_start();
+include("../inc/connection.php");
 
-	include("../inc/connection.php");
-
-    $points = $_POST['points'];
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['matchResult'])) {
+    $points = $_POST['matchResult'];
     $user_id = $_POST['user_id'];
     $match_id = $_POST['match_id'];
-    
-    
+
     $sql = "SELECT COUNT(*) as count FROM usuarios WHERE user_id = '$user_id'";
     $result = $conn->query($sql);
-    
-    if ($result && $result->fetch_assoc()['count'] > 0) {
-        
-        $sql_insert = "INSERT INTO Hist (match_id, user_id, points) VALUES ('$match_id', '$user_id', '$points')";
-        
-        if ($conn->query($sql_insert) === TRUE) {
-            echo "Pontos salvos com sucesso!";
-        } else {
-            echo "Erro ao salvar pontos: " . $conn->error;
-        }
-    } else {
-       
-        echo "Erro: O user_id fornecido não é válido.";
-    }
-    
-   
-    $conn->close();
 
+    if ($result && $result->fetch_assoc()['count'] > 0) {
+        $sql_insert = "INSERT INTO Hist (match_id, user_id, points) VALUES ('$match_id', '$user_id', '$points')";
+        $conn->query($sql_insert);
+    }
+}
+
+$conn->close();
 ?>
+
