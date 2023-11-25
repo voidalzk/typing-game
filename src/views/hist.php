5e1400139e6fc43d440663f723e0e3f28997fa5b
gabriel.voidaleski <gabriel.voidaleski@ufpr.br>
2023-11-25 17:27:33 -0300

    update

--- src/views/hist.php
+++ src/views/hist.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['matchResult'])) {
     $match_id = $_POST['match_id'];
 
     $sql = "SELECT COUNT(*) as count FROM usuarios WHERE user_id = '$user_id'";
-    $result = $conn->query($sql);
+    $result = $con->query($sql);
 
     if ($result && $result->fetch_assoc()['count'] > 0) {
         $sql_insert = "INSERT INTO historic (match_id, user_id, points) VALUES ('$match_id', '$user_id', '$points')";
@@ -16,7 +16,68 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['matchResult'])) {
     }
 }
 
-$conn->close();
+$con->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Histórico do Usuário</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 80%;
+            margin: 20px auto;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+
+<h2>Histórico do Usuário</h2>
+
+<?php
+
+include("../inc/connection.php");
+
+if (!isset($_SESSION['user_id'])) {
+     header("Location: login.php");
+     exit();
+}
+
+$user_id = $_SESSION['user_id'];
+
+$sql = "SELECT * FROM historic WHERE user_id = '$user_id'";
+$result = $con->query($sql);
+
+if ($result && $result->num_rows > 0) {
+    echo "<table>";
+    echo "<tr><th>Match ID</th><th>Pontos</th></tr>";
+    
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['match_id'] . "</td>";
+        echo "<td>" . $row['points'] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "<p>Nenhum histórico disponível.</p>";
+}
+
+$con->close();
 ?>
 
 </body>
